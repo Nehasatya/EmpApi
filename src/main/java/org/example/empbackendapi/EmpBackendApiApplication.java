c24d63c957312a4cbe74828e57de378a2534bee8
Neha Satya <nehasatya02@gmail.com>
2024-07-22 11:16:28 +0530

    inital commit

---
+++ src/main/java/org/example/empbackendapi/EmpBackendApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.empbackendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/empbackendapi/EmpBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.empbackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
